8fc3740a0ddaf6fc298faac50cd640e081497338
Никита <antyu.nikita550@gmail.com>
2023-11-17 16:29:50 +0500

    Добавьте файлы проекта.

---
+++ Models/ChopContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chop.Models;
+
+public partial class ChopContext : DbContext
+{
+    public ChopContext()
+    {
+    }
+
+    public ChopContext(DbContextOptions<ChopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<SecurityGuard> SecurityGuards { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Server=192.168.2.112;Database=chop;user=root");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("Client");
+
+            entity.Property(e => e.Adress).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<SecurityGuard>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("SecurityGuard");
+
+            entity.Property(e => e.Adress).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chop.Models;
+
+public partial class Client
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Adress { get; set; }
+}
