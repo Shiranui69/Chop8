50c01920b9775ea3022de888df5127f159c21fd6
Никита <antyu.nikita550@gmail.com>
2023-11-20 15:11:06 +0500

    thtfj

--- Controllers/CllientsController.cs
+++ Controllers/CllientsController.cs
@@ -4,8 +4,50 @@ using Chop.Models;
 
 namespace Chop.Controllers
 {
-    [ApiController]
-    public class CllientsController : Controller
+    [ ApiController]
+    [Route("/Clients")]
+    public class ClientsController : Controller
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var db = new ChopContext();
+            return Ok(db.Clients);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var db = new ChopContext();
+            var clo = db.Clients.SingleOrDefault(s => s.Id == id);
+            if (clo == null)
+                return NotFound();
+            return Ok(clo);
+        }
+        [HttpPost]
+        public IActionResult Add(Client client)
+        {
+            var db = new ChopContext();
+            db.Clients.Add(client);
+            db.SaveChanges();
+            return Ok(client);
+        }
+        [HttpPut]
+        public IActionResult Edit(Client clis)
+        {
+            var db = new ChopContext();
+            db.Clients.Update(clis);
+            db.SaveChanges();
+            return Ok(clis);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var db = new ChopContext();
+            var client = db.Clients.SingleOrDefault(s => s.Id == id);
+            if (client == null)
+                return NotFound();
+            return Ok(client);
+        }
     }
 }
